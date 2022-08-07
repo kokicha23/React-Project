8d138c37252b23c834e07683db88d656cd0a5f99
kmarinov <smileforfeed@abv.bg>
2022-08-07 12:58:43 +0300

    small css modifications

--- src/components/catalog/Catalog.js
+++ src/components/catalog/Catalog.js
@@ -1,11 +1,14 @@
 import { Card } from "./card/Card"
-
+import "./Catalog.css"
 
 export const Catalog = () => {
 
     return (
-        <>
+        <div className="catalog-wrapper">
             <Card />
-        </>
+            <Card />
+            <Card />
+            <Card />
+        </div>
     )
 }
